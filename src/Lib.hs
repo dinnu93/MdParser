f850811d23b894a2fddc84a47d7e2174518e631a
Dinesh Padisetti <dinesh.padisetti@gmail.com>
2019-02-18 13:50:29 +0530

    Edited the data structure

--- src/Lib.hs
+++ src/Lib.hs
@@ -9,37 +9,21 @@ import Control.Applicative ((<|>))
 
 -- Types
 
-type Document = [Block]
+data Document = Document [Block] deriving Show
 
 
 -- Block level types
 
 data Block = Heading Int [Inline]
            | Para [Inline]
-           | Blank
-           | Blockquote [Block]
-           | List ListType [[Block]]
-           | CodeBlock (Maybe String) String
-           | HtmlBlock String
-           | HRule
            deriving (Show,Eq)
-
-data ListType = Bullet Char | Numbered NumWrapper Int deriving (Show,Eq)
-
-data NumWrapper = PeriodFollowing | ParenFollowing | ParensAround
-                deriving (Show,Eq)
 
 -- Inline types
 
 data Inline = Text String
-            | Space
-            | SoftBreak
-            | LineBreak
             | Emph [Inline]
             | Strong [Inline]
-            | Code String
-            | Link [Inline] String
-            | Image [Inline] String
+            | Break
             deriving (Show,Eq)
             
 -- Parser
@@ -47,20 +31,27 @@ data Inline = Text String
 type Parser = Parsec Void String
 
 parseDocument :: Parser Document
-parseDocument = parseBlock `sepBy` ((string "\n\n") <|> (string "\n"))
+parseDocument = do
+  ls <- some parseBlock
+  return $ Document ls
 
 parseBlock :: Parser Block
-parseBlock = try parseHeading <|> parseParagraph
+parseBlock = choice [try parseHeading, parseParagraph]
 
 parseParagraph :: Parser Block
 parseParagraph = do
+  void (many eol)
   str <- some (anySingleBut '\n')
+  choice [eof, void (string "\n\n")]
   return $ Para [Text str]
 
 parseHeading :: Parser Block
 parseHeading = do
+  void (many eol)
   hashes <- some (char '#')
   guard (length hashes <= 6)
-  void spaceChar
+  void (some spaceChar)
   str <- some (anySingleBut '\n')
+  choice [eof, void (char '\n')]
+  void (many eol)
   return $ Heading (length hashes) [Text str]
